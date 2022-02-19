dd86da52a73d15df9cd312f6e0b8b57c12660aaa
Preetham Lawrence Veigas <preetham.lawrence@gmail.com>
2022-02-20 05:07:49 +0530

    updated header title

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            MyApp
           </Typography>
           <Button
             color="primary"
