ef94ce29746260ff17312539312b9ab82d651b48
Preetham Lawrence Veigas <preetham.lawrence@gmail.com>
2022-02-21 13:21:49 +0530

    change the title

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home page
+              UI Engineering Team
             </Typography>
             <Typography
               variant="h5"
