4dfeeea562732913a4f51f073481a5a47ff539ce
Preetham Lawrence Veigas <preetham.lawrence@gmail.com>
2022-05-31 08:45:39 +0530

    title change

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              DTI FED Team
+              DTI Team
             </Typography>
             <Typography
               variant="h5"
